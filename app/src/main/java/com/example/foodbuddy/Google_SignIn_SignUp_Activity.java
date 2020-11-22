062fa574fe25b87fe988885d8870d48b176b4497
Elisée  Djapa <43712965+elisee10@users.noreply.github.com>
2020-11-22 14:30:44 -0500

    Login And SignUp

---
+++ app/src/main/java/com/example/foodbuddy/Google_SignIn_SignUp_Activity.java
@@ -0,0 +1,14 @@
+package com.example.foodbuddy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Google_SignIn_SignUp_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google__sign_in__sign_up_);
+    }
+}
