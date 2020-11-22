33c7e00872a5f54bfbf7621f939a7ca30c4fdb3a
Elisée  Djapa <43712965+elisee10@users.noreply.github.com>
2020-11-22 18:02:42 -0500

    Login And SignUp

--- app/src/main/java/com/example/foodbuddy/LoginActivity.java
+++ app/src/main/java/com/example/foodbuddy/LoginActivity.java
@@ -2,14 +2,19 @@ package com.example.foodbuddy;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.RelativeLayout;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
 
-    private RelativeLayout mRLSignUp;
+    private EditText mEtEmail, mEtPassword;
+    private TextView mTvLogin;
+    private FirebaseAuth mFirebaseAuth;
 
 
 
@@ -18,13 +23,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mRLSignUp = findViewById(R.id.rl_signin_signin);
-        mRLSignUp.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
-            }
 
-        });
-    }
-}
+        mEtEmail = findViewById(R.id.et_email_signup);
+        mEtPassword = findViewById(R.id.et_pass_signup);
+        mTvLogin = findViewById(R.id.tv_signin_signin);
+
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mTvLogin.setOnClickListener(view -> {
+            String email = mEtEmail.getText().toString();
+            String pass = mEtPassword.getText().toString();
+
+            if (email.isEmpty() && !pass.isEmpty()) {
+                mFirebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        //Sign in complete
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();
+                    } else {
+                        //Something went wrong
+                        Toast.makeText(LoginActivity.this, "Please enter values", Toast.LENGTH_SHORT).show();
+                    }
+
+                })
+                        .addOnFailureListener(e -> Toast.makeText(LoginActivity.this, "Error: " + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show())
+
+                        .addOnCanceledListener(() -> Toast.makeText(LoginActivity.this, "Cancelled!", Toast.LENGTH_SHORT).show());
