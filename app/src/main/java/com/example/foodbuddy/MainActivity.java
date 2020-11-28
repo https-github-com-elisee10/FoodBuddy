4e0f4b5217120013d1a40b3a6bbc733b9b1ffd8f
norbeen <norbeen3@gmail.com>
2020-11-27 19:40:23 -0500

    navigation added

--- app/src/main/java/com/example/foodbuddy/MainActivity.java
+++ app/src/main/java/com/example/foodbuddy/MainActivity.java
@@ -3,20 +3,32 @@ package com.example.foodbuddy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.foodbuddy.fragments.profileFragment;
+import com.example.foodbuddy.fragments.searchFragment;
+import com.example.foodbuddy.fragments.trendingFragment;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
-    Button btn_logout;
+    private Button btn_logout;
+    private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
 
 
     @Override
@@ -24,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_logout = findViewById(R.id.btn_logout);
+        btn_logout = findViewById(R.id.btnLogOut);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
 
 
 
@@ -37,9 +51,43 @@ public class MainActivity extends AppCompatActivity {
 
         if (account!= null){
 
-            Log.d("Email", "email "+ account.getEmail());
+            //show the bottom navigation views
+
+
         }
 
+
+
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.profile:
+                        fragment = new profileFragment();
+                        break;
+                    case R.id.trending:
+                        fragment = new trendingFragment();
+                        break;
+                    case R.id.search:
+                        fragment = new searchFragment();
+                        break;
+                    default:
+                        fragment = new profileFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+
+        //set the profile to be default selection
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+
+
+//
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,6 +96,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent i = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(i);
+                finish();
             }
         });
     }
